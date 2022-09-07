3fb7b87b28a26323521e8691cbcba57092bb2545
arpinedaniel <112758149+arpinedaniel@users.noreply.github.com>
2022-09-07 21:20:42 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      items: 1,
+      margin:10,
+      loop: true,      
+      dots: true,
+      autoplay:false,
+      autoplayTimeout:1000,
+      autoplayHoverPause: true,
+
+  });
+});
+
